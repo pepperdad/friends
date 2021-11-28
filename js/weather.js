e222a2ee7aade755f00928502404db62530ee799
pepperdad <58854041+pepperdad@users.noreply.github.com>
2021-11-28 15:23:49 +0900

    final????

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const API_KEY = "e9ebe45a1dd4c6a232f2a465c2740c65";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
   .then(response =>response.json()
   .then(data =>{
